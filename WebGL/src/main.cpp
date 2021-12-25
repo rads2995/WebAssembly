50534b7ca87af2a0ca59c2e60903d2522b66a42d
Rodrigo Duran <Rads2995@gmail.com>
2021-12-25 03:29:36 -0500

    Starting over with WebGL 2 / OpenGL ES 3

---
+++ WebGL/src/main.cpp
@@ -0,0 +1,5 @@
+#include <GLES2/gl2.h>
+
+int main() {
+    return 0;
+}
--- WebGL/update.sh
+++ WebGL/update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-rm -rf /home/omega/Projects/rads2995.github.io/*
 cd /home/omega/Projects/WebAssembly/OpenGL/build/src
-cp *.html *.js *.wasm /home/omega/Projects/rads2995.github.io/
+cp *.js *.wasm /home/omega/Projects/rads2995.github.io/
 cd /home/omega/Projects/rads2995.github.io
 git add .
 git commit -m "Update from C++ Webassembly"
